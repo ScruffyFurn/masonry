dffadd1263fec79af08f09c12139fd6e028ed011
Mickey <mimacdo@microsoft.com>
2022-11-24 00:17:53 -0500

    Add: Simplified with Base image

--- create_new_library.sh
+++ create_new_library.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo -e "### DataBrick Python Library Creation ###\n\n"
 echo "Please enter required informationn"
-cookiecutter .
+cookiecutter https://github.com/ScruffyFurn/masonry-slug
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/__init__.py
+++
@@ -1,2 +0,0 @@
-# This line of code will allow shorter imports
-from {{cookiecutter.project_slug}}.{{cookiecutter.project_slug}} import {{cookiecutter.project_slug}} # noqa
